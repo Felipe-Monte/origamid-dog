94edfe7bda314fa02b921e1bcad3046193930722
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2025-01-09 21:49:31 -0300

    Feat: create context

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,43 +3,21 @@ import { Link } from 'react-router-dom';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST, USER_GET } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
   //Utilização do hook useForm
   const username = useForm();
   const password = useForm();
 
-  //Verifica se ja tem token no localstorage, ja faz o fetch assim que entrar
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {
-      getUser(token);
-    }
-  }, []);
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     //Validação antes de fazer o fetch
     if (username.validate() && password.validate()) {
-      //Uso do token post da api.jsx
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,20 @@ import Login from './Components/Login/Login';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
